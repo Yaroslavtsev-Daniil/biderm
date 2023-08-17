170ce57e0c7501562d349ca08d5beecf83ed5d37
User A0814665 <a0814665@alfheim.from.sh>
2023-08-17 03:39:10 +0300

    fix settings on prod

--- bcalc/bcalc/settings.py
+++ bcalc/bcalc/settings.py
@@ -122,9 +122,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = '/home/a0814665/domains/shkafy-bidermaier.ru/biderm/static'
+
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
-    ]
+     '/home/a0814665/domains/shkafy-bidermaier.ru/biderm/bcalc/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
